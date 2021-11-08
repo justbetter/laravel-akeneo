72a8d10b4da030c39c8beba3f477123a33614a20
Quinten Buis <quintenbuis@users.noreply.github.com>
2021-11-08 10:52:05 +0000

    Apply fixes from StyleCI

--- src/Events/ProductModelUpdated.php
+++ src/Events/ProductModelUpdated.php
@@ -2,10 +2,6 @@
 
 namespace JustBetter\Akeneo\Events;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
 class ProductModelUpdated extends AkeneoEvent
 {
 }
