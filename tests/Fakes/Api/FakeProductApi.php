5a7962f028925ad67b2d1489855bb1c5bc7fddba
Quinten Buis <quintenbuis@users.noreply.github.com>
2021-11-08 13:51:13 +0000

    Apply fixes from StyleCI

--- tests/Fakes/Api/FakeProductApi.php
+++ tests/Fakes/Api/FakeProductApi.php
@@ -24,46 +24,46 @@ class FakeProductApi
         }
 
         return [
-            "identifier" => "test",
-            "enabled"    => true,
-            "family"     => "stadsfietsen",
-            "categories" => [],
-            "groups"     => [],
-            "parent"     => null,
+            'identifier' => 'test',
+            'enabled'    => true,
+            'family'     => 'stadsfietsen',
+            'categories' => [],
+            'groups'     => [],
+            'parent'     => null,
 
-            "values" => [
-                "product_name" => [
+            'values' => [
+                'product_name' => [
                     [
-                        "locale" => "nl_NL",
-                        "scope"  => "magento",
-                        "data"   => "testing product",
+                        'locale' => 'nl_NL',
+                        'scope'  => 'magento',
+                        'data'   => 'testing product',
                     ],
                 ],
             ],
 
-            "created" => "2021-11-08T13:10:42+01:00",
-            "updated" => "2021-11-08T13:10:53+01:00",
+            'created' => '2021-11-08T13:10:42+01:00',
+            'updated' => '2021-11-08T13:10:53+01:00',
 
-            "associations"            => [
-                "UPSELL" => [
-                    "products"       => [],
-                    "product_models" => [],
-                    "groups"         => [],
+            'associations'            => [
+                'UPSELL' => [
+                    'products'       => [],
+                    'product_models' => [],
+                    'groups'         => [],
                 ],
 
-                "X_SELL" => [
-                    "products"       => [],
-                    "product_models" => [],
-                    "groups"         => [],
+                'X_SELL' => [
+                    'products'       => [],
+                    'product_models' => [],
+                    'groups'         => [],
                 ],
 
-                "SUBSTITUTION" => [
-                    "products"       => [],
-                    "product_models" => [],
-                    "groups"         => [],
+                'SUBSTITUTION' => [
+                    'products'       => [],
+                    'product_models' => [],
+                    'groups'         => [],
                 ],
             ],
-            "quantified_associations" => [],
+            'quantified_associations' => [],
         ];
     }
 
@@ -90,7 +90,8 @@ class FakeProductApi
     public function delete(string $code): int
     {
         $this->delete = $code;
-        return (int)($code === 'test');
+
+        return (int) ($code === 'test');
     }
 
     public function upsertList($resources): \Traversable
