11f6ed6b09142bbaa2a7f2c261daf86be86f00e3
Vincent Boon <vincent@justbetter.nl>
2021-11-22 10:38:17 +0100

    Add tests

---
+++ tests/Fakes/Api/FakeChannelApi.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace JustBetter\Akeneo\Tests\Fakes\Api;
+
+use Akeneo\Pim\ApiClient\Exception\NotFoundHttpException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\LazyCollection;
+
+class FakeChannelApi
+{
+    public array $all = [];
+    public array $upsert = [];
+    public string $delete = '';
+
+    public function create(string $code, array $data = []): int
+    {
+        //
+    }
+
+    public function get(string $code): array
+    {
+        if ($code !== 'test') {
+            throw new NotFoundHttpException('test', new Request('GET', '/'), new Response());
+        }
+
+        return [
+            'code' => 'test',
+            'currencies'    => [
+                'EUR',
+                'USD'
+            ],
+            'locales'     => [
+                'nl_NL',
+                'de_DE',
+                'en_US'
+            ],
+            'category_tree' => 'master',
+            'conversion_units'     => [
+                'weight' => 'KILOGRAM'
+            ],
+            'labels'     => [
+                'nl_NL' => 'Test nl_NL',
+                'de_DE' => 'Test de_DE',
+                'en_US' => 'Test en_US'
+            ],
+
+        ];
+    }
+
+    public function listPerPage(int $limit = 10, bool $withCount = false, array $queryParameters = [])
+    {
+        //
+    }
+
+    public function all(int $pageSize = 10, array $queryParameters = [])
+    {
+        $this->all['query'] = $queryParameters;
+
+        return LazyCollection::times($pageSize, function () {
+            return ['code' => '::test::'];
+        });
+    }
+
+    public function delete(string $code): int
+    {
+        $this->delete = $code;
+
+        return (int) ($code === 'test');
+    }
+
+    public function upsertList($resources): \Traversable
+    {
+        //
+    }
+}
